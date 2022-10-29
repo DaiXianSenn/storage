78f62cad078176874539a1d424216a2416e3ce6e
JhonDai <2404000810@qq.com>
2022-10-29 14:27:58 +0800

    test

--- src/main/java/www/DCW/storage/service/LoginService.java
+++ src/main/java/www/DCW/storage/service/LoginService.java
@@ -1,7 +1,10 @@
 package www.DCW.storage.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import www.DCW.storage.common.R;
+import www.DCW.storage.dto.UserDto;
 import www.DCW.storage.entity.Users;
+import www.DCW.storage.vo.UserVo;
 
 /**
  * Author: JhonDai
@@ -10,5 +13,8 @@ import www.DCW.storage.entity.Users;
  * Description:
  */
 
-public interface UserService extends IService<Users> {
+public interface LoginService extends IService<Users> {
+
+
+    public UserVo doLoginByUsername(Users users);
 }
---
+++ src/main/java/www/DCW/storage/vo/UserVo.java
@@ -0,0 +1,19 @@
+package www.DCW.storage.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import www.DCW.storage.entity.Users;
+
+/**
+ * Author: JhonDai
+ * Date: 2022/10/28/14:27
+ * Version: 1.0
+ * Description:
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserVo {
+    private Users users;
+}
